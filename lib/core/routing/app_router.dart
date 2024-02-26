8a8e410bf524190eab723d519d62c64e403ca78e
Fahd Mekawy <40622301+fahdmekawy@users.noreply.github.com>
2024-02-27 01:52:35 +0200

    - refactoring code - added regex file - added email and password validations

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -10,7 +10,7 @@ import '../di/dependency_injection.dart';
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
     //this arguments to be passed in any screen like this ( arguments as ClassName )
-    final arguments = settings.arguments;
+    // final arguments = settings.arguments;
 
     switch (settings.name) {
       case Routes.onBoardingScreen:
